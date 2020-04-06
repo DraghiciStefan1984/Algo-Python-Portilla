b3c6635d033e87381386330296655151fd186091
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-04-06 09:21:20 +0300

    pair sum

--- arrays/arrays_problems.py
+++ arrays/arrays_problems.py
@@ -24,3 +24,17 @@ def anagram_check_2(s1, s2):
         if count[k]!=0:
             return False
     return True
+
+
+def array_pair_sum(arr, k):
+    if len(arr)<2:
+        return
+    seen=set()
+    output=set()
+    for num in arr:
+        target=k-num
+        if target not in seen:
+            seen.add(num)
+        else:
+            output.add((min(num, target), max(num, target)))
+    return '\n'.join(map(str, list(output)))
