aa8d4306f296fe10037451af373f947410ff90c6
DraghiciStefan1984 <draghicistefan1984@gmail.com>
2020-04-07 08:01:32 +0300

    arrays problems continued

--- arrays/arrays_problems.py
+++ arrays/arrays_problems.py
@@ -1,3 +1,6 @@
+import collections
+
+
 def anagram_check_1(s1, s2):
     s1=s1.replace(' ', '').lower()
     s2=s2.replace(' ', '').lower()
@@ -38,3 +41,33 @@ def array_pair_sum(arr, k):
         else:
             output.add((min(num, target), max(num, target)))
     return '\n'.join(map(str, list(output)))
+
+
+def find_missing_element_1(arr1, arr2):
+    arr1.sort()
+    arr2.sort()
+    for num1, num2 in zip(arr1, arr2):
+        if num1!=num2:
+            return num1
+    return arr1[-1]
+
+
+def find_missing_element_2(arr1, arr2):
+    d=collections.defaultdict(int)
+    for num in arr1:
+        d[num]+=1
+    for num in arr2:
+        if d[num]==0:
+            return num
+        else:
+            d[num]-=1
+
+
+def largest_continuous_sum(arr):
+    if len(arr)==0:
+        return 0
+    max_sum=curent_sum=arr[0]
+    for num in arr[1:]:
+        curent_sum=max(curent_sum+num, num)
+        max_sum=max(curent_sum, max_sum)
+    return max_sum
